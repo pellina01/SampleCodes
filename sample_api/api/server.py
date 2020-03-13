1fddbe2ef457d8ba725fb4e4e92967d3ea7c7258
pellina01 <pellina01@github.com>
2020-03-13 10:56:37 +0800

    not sir dalisay code
    ngrok.exe included

--- sample_api/api/server.py
+++ sample_api/api/server.py
@@ -1,16 +1,13 @@
 from flask import Flask, request
+import json
+import ast
 
 app = Flask(__name__)
 
-FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
-VERIFY_TOKEN = 'any_password'# <paste your verify token here>
-PAGE_ACCESS_TOKEN = 'daa86080cd49f275113af7198fe4fe0a'# paste your page access token here>"
+FB_API_URL = 'https://graph.facebook.com/v6.0/me/messages'
+VERIFY_TOKEN = 'any_password'
+PAGE_ACCESS_TOKEN = 'daa86080cd49f275113af7198fe4fe0a'
 
-
-def get_bot_response(message):
-    """This is just a dummy function, returning a variation of what
-    the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
 
 
 def verify_webhook(req):
@@ -19,37 +16,24 @@ def verify_webhook(req):
     else:
         return "incorrect"
 
-def respond(sender, message):
-    """Formulate a response to the user and
-    pass it on to a function that sends it."""
-    response = get_bot_response(message)
-    send_message(sender, response)
 
-
-def is_user_message(message):
-    """Check if the message is a message from the user"""
-    return (message.get('message') and
-            message['message'].get('text') and
-            not message['message'].get("is_echo"))
-
-
-@app.route("/webhook")
+@app.route("/webhook", methods = ['GET', 'POST'])
 def listen():
-    """This is the main function flask uses to 
-    listen at the `/webhook` endpoint"""
     if request.method == 'GET':
         return verify_webhook(request)
 
     if request.method == 'POST':
-        payload = request.json
-        event = payload['entry'][0]['messaging']
-        for x in event:
-            if is_user_message(x):
-                text = x['message']['text']
-                sender_id = x['sender']['id']
-                respond(sender_id, text)
+        output = request.get_json()
+ #       entry_val = output['entry']
+ #       entry_list = entry_val[0]
+ #       changes_val = entry_list['changes']
+  #      changes_list = changes_val[0]
+   #     value_val = changes_list['value']
+        print(output)
+        return 'im here'
+        
 
-        return "ok"
+
 
 if __name__=="__main__":
     app.run()
